c8f1ca6b81c80872c8aaabfdd1f1bfe217d1fa1b
mamoru_kijima <mamoru.kijima@dena.jp>
2016-07-14 23:41:22 +0900

    inboxページ 映画リスト表示をマークアップ

--- app/script/app.js
+++ app/script/app.js
@@ -1,11 +1,24 @@
+// ライブラリ
 import $ from 'jquery';
+import Vue from 'vue';
 
+// グローバル変数
 const TOUCH = ('ontouchstart' in document) ? 'touchstart' : 'click';
 
+// HTML読み込み時実行
+window.onload = () => {
+};
+
+// 変数
 const navButton = $('#nav-button');
 const nav       = $('#header-nav');
 
+// イベント
 navButton.on(TOUCH, function(e) {
   navButton.toggleClass('open');
   nav.toggleClass('open');
+  // ナビを開いた時はスクロールさせない
+  $(window).on('touchmove.noScroll', function(e) {
+    e.preventDefault();
+  });
 });
