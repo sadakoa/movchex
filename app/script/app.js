83832de71701a7befaec8b7ff54adce9a39f7b05
mamoru_kijima <mamoru.kijima@dena.jp>
2016-07-15 11:21:52 +0900

    ルーティングを記述した

--- app/script/app.js
+++ app/script/app.js
@@ -7,3 +7,16 @@ import * as load from './load';
 window.onload = () => {
   load.setEvent();
 };
+
+// ルーティング
+new Vue({
+  el: '#app',
+  methods: {
+    inbox: function () {
+      window.location.href = 'index.html';
+    },
+    search: function () {
+      window.location.href = 'search.html';
+    },
+  },
+});
