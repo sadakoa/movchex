ef4bb07551dbfdcd0662fc2ea4089bff7112319c
mamoru_kijima <mamoru.kijima@dena.jp>
2016-07-16 16:33:49 +0900

    adaptive background.jsの実行処理を実装

--- app/script/app.js
+++ app/script/app.js
@@ -25,3 +25,21 @@ let routes = new Vue({
     },
   },
 });
+
+
+/**
+ * jquery.adaptive-backgrounds.jsの実行関数
+ */
+const adaptive_params = {
+  selector: '.p-movie-info__image',
+  // 背景色を適用する対象セレクタ
+  parent: '.l-work-main',
+  // 背景色の明度に合わせてフォントカラーを変更するかどうか
+  normalizeTextColor: true,
+  // 明度に合わせたフォントカラー
+  normalizedTextColors: {dark: '#000', light: '#fff'},
+  // 明度に合わせたクラス適用
+  lumaClasses: {light: 'ab-light', dark: 'ab-dark'}
+};
+// 実行
+$.adaptiveBackground.run(adaptive_params);
