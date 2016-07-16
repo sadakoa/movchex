28c78c668ae646972282149efe3e1f2e7ed89848
mamoru_kijima <mamoru.kijima@dena.jp>
2016-07-16 11:15:07 +0900

    CSSを圧縮するタスクを追加

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -1,9 +1,8 @@
 import gulp from 'gulp';
 import notify from 'gulp-notify';
 import plumber from 'gulp-plumber';
-import autoprefixer from 'gulp-autoprefixer';
 import stylus from 'gulp-stylus';
-import raname from 'gulp-rename';
+import pleeease from 'gulp-pleeease';
 import browser from 'browser-sync';
 const conf = require('../config.js');
 
@@ -11,8 +10,9 @@ gulp.task('stylus', () => {
  return gulp.src('app/stylus/app.styl')
 .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
 .pipe(stylus())
-.pipe(autoprefixer())
-.pipe(raname('app.css'))
+.pipe(pleeease({
+  out: 'app.min.css',
+}))
 .pipe(gulp.dest(conf.stylus.dest))
 .pipe(browser.reload({stream:true}));
 });
