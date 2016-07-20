42535d9d210236fa9071a0fe177a59145a06d2dd
mamoru_kijima <mamoru.kijima@dena.jp>
2016-07-20 10:18:47 +0900

    空入力するとエラーメッセージを出すように実装

--- app/script/Controller/send.js
+++ app/script/Controller/send.js
@@ -17,7 +17,7 @@ export const inputKeyword =  new Vue({
   methods: {
     sendKeyword: function() {
       // inputがからの場合は処理を終了する
-      if (this.keyword === '') { return; }
+      if (this.keyword === '') { alert('検索キーワードを入力してください');return; }
       let keyword = this.keyword.trim();
       movie.getKeywordMovies(keyword);
     },
