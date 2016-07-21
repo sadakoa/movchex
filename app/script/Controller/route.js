3b623e82501ea988f8e2594bfc3edd8e0f47e9b6
mamoru_kijima <mamoru.kijima@dena.jp>
2016-07-21 15:24:18 +0900

    作品ページのみ解析処理を行うように変更

--- app/script/Controller/route.js
+++ app/script/Controller/route.js
@@ -5,6 +5,8 @@
 // 利用モジュール & パッケージ =====================================
 import * as movie from '../Model/movie';
 import * as convert from '../Model/convert';
+import * as check from '../Model/check';
+import * as storage from '../Model/storage';
 // =============================================================
 
 /**
@@ -40,8 +42,19 @@ export function urlDispatcher() {
   if (mainId === 'work-main') {
     // URLのパラメータを変数に格納
     const urlPair = Number(location.search.substring(1).split('&'));
+    const inboxArray = storage.getStorage();
+
+    // ストレージデータが存在するならデータをチェックする処理を行う
+    if (inboxArray == null) {
+      console.log('Not a data :)');
+    } else {
+      check.seekIdMovies(urlPair, inboxArray);
+    }
+
+    // 映画情報を取得する処理
     movie.getIdMovie(urlPair);
 
+    // おすすめの作品に関する処理
     const randomNum = convert.showRandomNum();
     movie.getRandomPopularMovies(randomNum);
   } else {
